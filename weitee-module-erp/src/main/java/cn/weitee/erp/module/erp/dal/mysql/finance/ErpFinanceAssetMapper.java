53818dd67db35e5f6f106e9ef86b70f2c1de29dc
longchen <1364036191@qq.com>
2026-07-16 08:29:23 +0800

    feat(finance): scope finance asset access

--- weitee-module-erp/src/main/java/cn/weitee/erp/module/erp/dal/mysql/finance/ErpFinanceAssetMapper.java
+++ weitee-module-erp/src/main/java/cn/weitee/erp/module/erp/dal/mysql/finance/ErpFinanceAssetMapper.java
@@ -8,17 +8,27 @@ import cn.weitee.erp.module.erp.dal.dataobject.finance.ErpFinanceAssetDO;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Collection;
+
 @Mapper
 public interface ErpFinanceAssetMapper extends BaseMapperX<ErpFinanceAssetDO> {
 
     default PageResult<ErpFinanceAssetDO> selectPage(ErpFinanceAssetPageReqVO reqVO) {
-        return selectPage(reqVO, new LambdaQueryWrapperX<ErpFinanceAssetDO>()
+        return selectPage(reqVO, buildPageQuery(reqVO));
+    }
+
+    default PageResult<ErpFinanceAssetDO> selectPageByDeptIds(ErpFinanceAssetPageReqVO reqVO, Collection<Long> deptIds) {
+        return selectPage(reqVO, buildPageQuery(reqVO).in(ErpFinanceAssetDO::getDeptId, deptIds));
+    }
+
+    private LambdaQueryWrapperX<ErpFinanceAssetDO> buildPageQuery(ErpFinanceAssetPageReqVO reqVO) {
+        return new LambdaQueryWrapperX<ErpFinanceAssetDO>()
                 .likeIfPresent(ErpFinanceAssetDO::getNo, reqVO.getNo())
                 .likeIfPresent(ErpFinanceAssetDO::getName, reqVO.getName())
                 .likeIfPresent(ErpFinanceAssetDO::getCategoryName, reqVO.getCategoryName())
                 .eqIfPresent(ErpFinanceAssetDO::getAssetType, reqVO.getAssetType())
                 .eqIfPresent(ErpFinanceAssetDO::getStatus, reqVO.getStatus())
-                .orderByDesc(ErpFinanceAssetDO::getId));
+                .orderByDesc(ErpFinanceAssetDO::getId);
     }
 
     default ErpFinanceAssetDO selectByNo(String no) {
